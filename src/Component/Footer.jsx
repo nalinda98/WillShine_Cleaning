09c319dc154094acefcd61c355c38077d5e8e84c
Nalinda Deshapriya <nalindadkumarasinghe@gmail.com>
2025-04-24 21:01:13 +0530

    navbar updated

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+'use client'
+
+import React, { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
+  const [showModal, setShowModal] = useState(false);
+
+  const handleShow = () => setShowModal(true);
+  const handleClose = () => setShowModal(false);
 
   return (
     <div>
@@ -9,125 +17,68 @@ const Footer = () => {
         <div className="container">
           <div className="footer-top-box">
             <div className="row">
-              <div className="col-lg-3 col-sm-6">
+              {/* Features Section */}
+              <div className="col-lg-4 col-sm-6">
                 <div className="footer-box-layout1">
                   <div className="footer-title">
                     <h4>FEATURES</h4>
                   </div>
-                  <div className="footer-menu-box">
-                    <ul className="footer-menu-list">
-                      <li>
-                        <a href="#">Residential Services</a>
-                      </li>
-                      <li>
-                        <a href="#">Commercial Services</a>
-                      </li>
-                      <li>
-                        <a href="#">Vechile Wash</a>
-                      </li>
-                      <li>
-                        <a href="#">Londry Facilities</a>
-                      </li>
-                      <li>
-                        <a href="#">Carpet Removal</a>
-                      </li>
-                    </ul>
-                  </div>
+                  <ul className="footer-menu-list">
+                    <li><a href="#">Residential Services</a></li>
+                    <li><a href="#">Commercial Services</a></li>
+                    <li><a href="#">Vehicle Wash</a></li>
+                    <li><a href="#">Laundry Facilities</a></li>
+                    <li><a href="#">Carpet Removal</a></li>
+                  </ul>
                 </div>
               </div>
-              <div className="col-lg-3 col-sm-6">
+
+              {/* Company Section */}
+              <div className="col-lg-4 col-sm-6">
                 <div className="footer-box-layout1">
                   <div className="footer-title">
                     <h4>COMPANY</h4>
                   </div>
-                  <div className="footer-menu-box">
-                    <ul className="footer-menu-list">
-                      <li>
-                        <a href="about1.html">About Us</a>
-                      </li>
-                      <li>
-                        <a href="#">Testimonials</a>
-                      </li>
-                      <li>
-                        <a href="#">Terms</a>
-                      </li>
-                      <li>
-                        <a href="#">Media Kit</a>
-                      </li>
-                      <li>
-                        <a href="#">Sitemap</a>
-                      </li>
-                    </ul>
-                  </div>
+                  <ul className="footer-menu-list">
+                    <li><a href="about1.html">About Us</a></li>
+                    <li><a href="#">Testimonials</a></li>
+                    <li><a href="#">Terms</a></li>
+                    <li><a href="#">Media Kit</a></li>
+                    <li><a href="#">Sitemap</a></li>
+                  </ul>
                 </div>
               </div>
-              <div className="col-lg-3 col-sm-6">
+
+              {/* Quick Links Section */}
+              <div className="col-lg-4 col-sm-6">
                 <div className="footer-box-layout1">
                   <div className="footer-title">
                     <h4>QUICK LINKS</h4>
                   </div>
-                  <div className="footer-menu-box">
-                    <ul className="footer-menu-list">
-                      <li>
-                        <a href="#">Features</a>
-                      </li>
-                      <li>
-                        <a href="#">Pricing</a>
-                      </li>
-                      <li>
-                        <a href="#">Partners</a>
-                      </li>
-                      <li>
-                        <a href="#">Cloud Affiliate Program</a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-3 col-sm-6">
-                <div className="footer-box-layout1">
-                  <div className="footer-title">
-                    <h4>AFFILIATES</h4>
-                  </div>
-                  <div className="footer-menu-box">
-                    <ul className="footer-menu-list">
-                      <li>
-                        <a href="#">Become An Affiliate</a>
-                      </li>
-                      <li>
-                        <a href="#">Affiliate Portal</a>
-                      </li>
-                    </ul>
-                    {/* <a href="contact.html" className="contact-btn">
-                      <i className="far fa-envelope"></i>HAVE ANY QUESTION
-                    </a> */}
-                  </div>
+                  <ul className="footer-menu-list">
+                    <li><a href="#">Features</a></li>
+                    <li><a href="#">Pricing</a></li>
+                    <li><a href="#">Partners</a></li>
+                    <li><a href="#">Cloud Affiliate Program</a></li>
+                  </ul>
                 </div>
               </div>
             </div>
           </div>
+
+          {/* Footer Bottom */}
           <div className="footer-bottom-box">
             <div className="row">
               <div className="col-md-6">
                 <div className="copyright">
-                  All rights reserved | This website is Designed & Developed by <a style={{
-                    color:"white",
-                    fontWeight:"bold"
-                  }} href="https://buildzoneit.com/">BuildZone IT</a>
+                  © {currentYear} All rights reserved | Designed & Developed by 
+                  <a href="https://buildzoneit.com/" style={{ color: "white", fontWeight: "bold" }}> BuildZone IT</a>
                 </div>
               </div>
               <div className="col-md-6">
                 <div className="footer-bottom-menu">
                   <ul>
-                    {/* <li>
-                      <a href="#">Sitemap</a>
-                    </li> */}
-                    <li>
-                      <a href="#">Terms of Service</a>
-                    </li>
-                    <li>
-                      <a href="#">Privacy Policy</a>
-                    </li>
+                    <li><button className="btn btn-link p-0" onClick={handleShow}>Privacy Policy</button></li>
                   </ul>
                 </div>
               </div>
@@ -135,6 +86,44 @@ const Footer = () => {
           </div>
         </div>
       </footer>
+
+      {/* Privacy Policy Modal */}
+      <Modal show={showModal} onHide={handleClose} size="lg" scrollable>
+        <Modal.Header closeButton>
+          <Modal.Title>Privacy Policy</Modal.Title>
+        </Modal.Header>
+        <Modal.Body style={{ whiteSpace: "pre-wrap" }}>
+          <strong>Who we are</strong>
+          {"\n"}Suggested text: Our website address is: https://blossompropertyservices.com.au.
+
+          {"\n\n"}<strong>Comments</strong>
+          {"\n"}Suggested text: When visitors leave comments on the site we collect the data shown in the comments form...
+
+          {"\n\n"}<strong>Media</strong>
+          {"\n"}Suggested text: If you upload images to the website, you should avoid uploading images with embedded location data...
+
+          {"\n\n"}<strong>Cookies</strong>
+          {"\n"}Suggested text: If you leave a comment on our site you may opt-in to saving your name, email...
+
+          {"\n\n"}<strong>Embedded content from other websites</strong>
+          {"\n"}Suggested text: Articles on this site may include embedded content...
+
+          {"\n\n"}<strong>Who we share your data with</strong>
+          {"\n"}Suggested text: If you request a password reset, your IP address will be included...
+
+          {"\n\n"}<strong>How long we retain your data</strong>
+          {"\n"}Suggested text: If you leave a comment, the comment and its metadata are retained indefinitely...
+
+          {"\n\n"}<strong>What rights you have over your data</strong>
+          {"\n"}Suggested text: If you have an account on this site, or have left comments...
+
+          {"\n\n"}<strong>Where your data is sent</strong>
+          {"\n"}Suggested text: Visitor comments may be checked through an automated spam detection service.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>Close</Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
