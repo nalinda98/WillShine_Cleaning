d4e5a545aee075a1864da120cb2c30e7b88a4cc8
Nalinda Deshapriya <nalindadkumarasinghe@gmail.com>
2025-05-09 15:25:45 +0530

    aa

--- dist/static/chunks/src_app_about_page_jsx_29a9528a._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_swiper_00c7e98a._.js",
-  "static/chunks/src_Component_cd422caf._.js",
-  "static/chunks/node_modules_swiper_62da158d._.css"
-],
-    source: "dynamic"
-});
