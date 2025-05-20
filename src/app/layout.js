96c38402bb79cf1e9c64b71e80f088dee53ab2e3
Nalinda Deshapriya <nalindadkumarasinghe@gmail.com>
2025-05-20 18:28:28 +0530

    changed

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,7 @@ import "./styles/vendor/OwlCarousel/owl.carousel.min.css";
 import "./styles/vendor/OwlCarousel/owl.theme.default.min.css";
 import "./styles/css/select2.min.css";
 import "./styles/css/jquery.datetimepicker.css";
+import "./../../public/site.webmanifest.json";
 import Head from "next/head";
 import Script from "next/script";
 import Preloader from "@/Component/Preloader";
